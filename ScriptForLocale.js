bd9194d235efebcd5c58b971b3563b0d02b23d79
Gero7711 <oreg.gamarcha@gmail.com>
2024-05-08 10:07:55 +0800

    Please enter the commit message for your changes. Lines starting

--- ScriptForLocale.js
+++
@@ -1,8 +0,0 @@
-function searchClick(){
-    document.getElementById("Money").innerHTML = "On Going Page"
-}
-
-// data to get
-// cons sell_services = {service: "#", price: "#"}
-// const sell_products = {product: "#", price: "#"}
-//const user = {firstName: "#", lastName: "#" ,birthDate: "#" ,address: "#", }
